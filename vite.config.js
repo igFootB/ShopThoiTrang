49ca9e340de4f824ca192a4feed16f881e98bee4
Bảo Đường <duonggiabao1256@gmail.com>
2026-03-20 20:50:39 +0700

    Thêm Dockerfile cho backend và ai-service

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Cấu hình chuẩn cho Vercel
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Đổi lại thành '/' cho Vercel
-});
